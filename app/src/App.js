7ba1cce733a69015ca804f99517eb99bcb804186
yujin-bondarev <evgenij.bondarev.2004@mail.ru>
2024-09-23 10:22:12 +0200

    added App and Form

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import EmployeeAPI from "./api/service";
 import Table from "./Table";
+import Form from "./Form.js";
+import { useState } from "react";
+
+const initialEmployees = EmployeeAPI.all();
 
 function App() {
+  const [employees, setEmployees] = useState(initialEmployees);
+
+  const delEmp = (id) => {
+    if (EmployeeAPI.delete(id)) {
+      setEmployees(employees.filter((employee) => employee.id !== id));
+    }
+  };
+
+  const addEmployee = (employee) => {
+    const newEmployee = EmployeeAPI.add(employee);
+    if (newEmployee) {
+      setEmployees([...employees, newEmployee]);
+    }
+  };
+
   return (
     <div className="App">
-      <div className="App">
-        <Table employees={EmployeeAPI.all()} />
-      </div>
+      <Form handleSubmit={addEmployee} inEmployee={{ name: "", job: "" }} />
+      <Table employees={employees} delEmployee={delEmp} />
     </div>
   );
 }
